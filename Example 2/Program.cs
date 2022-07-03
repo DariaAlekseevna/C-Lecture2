b5ad92016b2d927adcd67fe3d801e6289e57ccae
Daria Alekseevna <dasha.gugnina98@gmail.com>
2022-07-03 06:32:57 +0300

    add exp.3 массив, макс из 9 чисел

--- Example 2/Program.cs
+++ Example 2/Program.cs
@@ -18,10 +18,15 @@ int Max(int arg1, int arg2, int arg3)
     return result;
 }
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
+
+int max =Max(
+    Max(a1, b1, c1), 
+    Max(a2, b2, c2), 
+    Max(a3, b3, c3));
 
 Console.WriteLine(max);
 
---
+++ Example 3/Program.cs
@@ -0,0 +1,21 @@
+﻿//нахождение максимума из 9 чисел, массив
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = {22, 54, 65, 41, 765, 23, 7, 43, 4822};
+array[0] = 5679;
+Console.WriteLine(array[5]);
+
+int max =Max(
+    Max(array[0], array[1], array[2]), 
+    Max(array[3], array[4], array[5]), 
+    Max(array[6], array[7], array[8])
+    );
+
+Console.WriteLine(max);
