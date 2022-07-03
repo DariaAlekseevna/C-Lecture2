517b2407383ea83649682341713b3e1fa3f11073
Daria Alekseevna <dasha.gugnina98@gmail.com>
2022-07-03 06:41:36 +0300

    add exp.4 найти элемент массива

---
+++ Example4/Program.cs
@@ -0,0 +1,18 @@
+﻿//найти элемент массива, равный find. Если array [index] = find, алгоритм завершил работу успешно.
+
+int[] array = { 33, 12, 65, 8, 98, 3, 432, 8 };
+
+int n = array.Length;
+int find = 8;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
