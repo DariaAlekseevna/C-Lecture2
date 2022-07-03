fc666b2dad9f5daed5353d1300e4002c4cfe5624
Daria Alekseevna <dasha.gugnina98@gmail.com>
2022-07-03 07:22:42 +0300

    add exp.6 нахождение позиции элемента массива из случайных чисел и печать массива

---
+++ Example6/Program.cs
@@ -0,0 +1,50 @@
+﻿//нахождение позиции элемента массива с выводом массива
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; //-1 значит не нейдено
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int [10]; //определили массив из 10 эл-тов
+
+FillArray(array); //вызвали функцию заполнения массива
+PrintArray(array); //вызвали функцию печати массива
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
